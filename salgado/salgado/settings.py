e8af150062b50e2f8d60a8a205014ec7c9850e3a
Pedro Henrique Kopper <pedro@kopper.dev>
2022-07-11 23:12:06 -0300

    add json endpoint

--- salgado/salgado/settings.py
+++ salgado/salgado/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = './media'
+MEDIA_URL = '/media/'
--- salgado/salgado/urls.py
+++ salgado/salgado/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from images.views import v1_get_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('v1/get_image', v1_get_image),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
